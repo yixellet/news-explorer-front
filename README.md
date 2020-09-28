4312cdc688bd30227bc686f4b1a0ecc88485efd6
Kirill Kotelevsky <yixwellet@gmail.com>
2020-09-28 15:57:08 +0400

    Выполнена настройка Webpack
    1. настроен webpack.config.js
    2. настроен Бабель
    3. Настроен PostCSS
    4. написаны скрипты для финальной сборки, для разработки и для деплоя на gh-pages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # news-explorer-front
 Фронтенд для дипломного проекта "News Explorer" студента Яндекс.Практикум Котелевского К. А.
+
+[https://yixellet.github.io/news-explorer-front/](https://yixellet.github.io/news-explorer-front/)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: { 
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
